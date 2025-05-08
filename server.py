92763a1f071ff7392f4221ec90b476bbd2d9659d
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-05-08 23:36:00 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("MCP Server Demo 🚀")
+mcp_server = FastMCP("MCP Server Demo 🚀")
 
-@mcp.tool()
+@mcp_server.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
-@mcp.tool()
+@mcp_server.tool()
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
     return a * b
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp_server.run(transport="sse", host="0.0.0.0", port=8000)
