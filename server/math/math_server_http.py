1251c4df8e56557bfc5cb2ca13d66279a7ca2c1f
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-08-27 20:51:19 +0800

    Update math_server_http.py

--- server/math/math_server_http.py
+++ server/math/math_server_http.py
@@ -1,24 +1,24 @@
 import asyncio, logging
 from fastmcp import FastMCP
 
-mcp_server = FastMCP(name="MathMCPServer")
+mcp = FastMCP(name="MathMCPServer")
 
 logging.basicConfig(level=logging.INFO)
 
-@mcp_server.tool()
+@mcp.tool()
 def add(a: int, b: int) -> int:
     """Adds two numbers"""
     logging.info(f"add() called with a={a} and b={b}")
     return a + b
 
-@mcp_server.tool()
+@mcp.tool()
 def multiply(a: int, b: int) -> int:
     """Multiplies two numbers"""
     logging.info(f"multiply() called with a={a} and b={b}")
     return a * b
 
 async def main():
-    await mcp_server.run_async(transport="streamable-http", path="/mcp", host="0.0.0.0", port=8000)
+    await mcp.run_async(transport="streamable-http", path="/mcp", host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
     asyncio.run(main())
